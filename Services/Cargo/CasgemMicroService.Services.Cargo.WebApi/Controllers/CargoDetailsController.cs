95883b73db788264fbf38c9ba140fc56cceabf42
oguzhan celik <oguzhancelik2027@gmail.com>
2023-08-15 09:42:14 +0300

    ....

---
+++ Services/Cargo/CasgemMicroService.Services.Cargo.WebApi/Controllers/CargoDetailsController.cs
@@ -0,0 +1,30 @@
+﻿using CasgemMicroService.Services.Cargo.BussinessLayer.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasgemMicroService.Services.Cargo.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CargoDetailsController : ControllerBase
+	{
+		private readonly ICargoDetailService _cargoDetailService;
+
+		public CargoDetailsController(ICargoDetailService cargoDetailService)
+		{
+			_cargoDetailService = cargoDetailService;
+		}
+		[HttpGet]
+		public IActionResult CargoDetailList()
+		{
+			var values = _cargoDetailService.GetAll();
+			return Ok(values);
+		}
+		[HttpGet("{id}")]
+		public IActionResult CargoDetailGet(int id)
+		{
+			var value = 
+		}
+	}
+}
+
